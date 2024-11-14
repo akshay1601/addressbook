9e4b1b199329c095b931773a92f59bfc0216a024
Akshay Sudhakar Virkhare <akshay.sudhakarvirkhare@rackspace.com>
2024-11-14 18:16:56 +0530

    updating jekinks files

--- server-comfig.sh
+++ server-comfig.sh
@@ -1,16 +1,16 @@
 #! /bin/bash 
-wget https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz
-sudo tar xf apache-maven-3.9.9-bin.tar.gz -C /opt
-touch maven.sh
-sudo echo "export JAVA_HOME=/usr/lib/jvm/default-java 
-export M3_HOME=/opt/apache-maven-3.9.9 
-export MAVEN_HOME=/opt/apache-maven-3.9.9
-export PATH=${M3_HOME}/bin:${PATH}" > maven.sh
-sudo scp maven.sh /etc/profile.d/
-sudo chmod +x /etc/profile.d/maven.sh
-source /etc/profile.d/maven.sh
+# wget https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz
+# sudo tar xf apache-maven-3.9.9-bin.tar.gz -C /opt
+# touch maven.sh
+# sudo echo "export JAVA_HOME=/usr/lib/jvm/default-java 
+# export M3_HOME=/opt/apache-maven-3.9.9 
+# export MAVEN_HOME=/opt/apache-maven-3.9.9
+# export PATH=${M3_HOME}/bin:${PATH}" > maven.sh
+# sudo scp maven.sh /etc/profile.d/
+# sudo chmod +x /etc/profile.d/maven.sh
+# source /etc/profile.d/maven.sh
 
 cd /home/ubuntu
-git clone https://github.com/akshay1601/addressbook.git
+git checkout https://github.com/akshay1601/addressbook.git
 cd addressbook
 mvn package
