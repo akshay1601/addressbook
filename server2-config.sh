868f0d65808db6f7e751295196a455c4096adaaa
Akshay Sudhakar Virkhare <akshay.sudhakarvirkhare@rackspace.com>
2024-11-14 20:18:35 +0530

    Script files

---
+++ server2-config.sh
@@ -0,0 +1,19 @@
+#! /bin/bash 
+
+if [ -d "/opt/apache-maven-3.9.9" ]; then 
+    echo "Mvaen is installed"
+else
+    wget https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz
+    sudo tar xf apache-maven-3.9.9-bin.tar.gz -C /opt
+fi 
+
+if [ -d "addressbook" ]; then 
+    echo "Repo is present"
+    cd /home/ubuntu/addressbook
+    git pull origin main
+else
+    git clone https://github.com/akshay1601/addressbook.git
+    cd addressbook
+fi
+
+/opt/apache-maven-3.9.9/bin/mvn  test
