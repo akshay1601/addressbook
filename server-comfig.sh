e302c0b1fc630357590f846ed9f4dc8f7475a835
Akshay Sudhakar Virkhare <akshay.sudhakarvirkhare@rackspace.com>
2024-11-14 18:59:55 +0530

    updating jekinks files

--- server-comfig.sh
+++ server-comfig.sh
@@ -1,20 +1,18 @@
 #! /bin/bash 
 
-if [-d "/opt/apache-maven-3.9.9"]
-then 
+if [-d "/opt/apache-maven-3.9.9"];then 
     echo "Mvaen is installed"
 else
     wget https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz
     sudo tar xf apache-maven-3.9.9-bin.tar.gz -C /opt
 
 
-# if [-d "addressbook"]
-# then 
-#     echo "Repo is present"
-#     cd /home/ubuntu/addressbook
-#     git pull origin main
-# else
-#     git clone https://github.com/akshay1601/addressbook.git
-#     cd addressbook
+if [-d "addressbook"];then 
+    echo "Repo is present"
+    cd /home/ubuntu/addressbook
+    git pull origin main
+else
+    git clone https://github.com/akshay1601/addressbook.git
+    cd addressbook
 
-# /opt/apache-maven-3.9.9/bin/mvn  package
+/opt/apache-maven-3.9.9/bin/mvn  package
