e50ad4a3f82b92dddf7967d3151c3084101a55c5
Akshay Sudhakar Virkhare <akshay.sudhakarvirkhare@rackspace.com>
2024-11-18 19:17:13 +0530

    Jenkin and script file update

--- server3-config.sh
+++ server3-config.sh
@@ -19,4 +19,5 @@ fi
 # /opt/apache-maven-3.9.9/bin/mvn  package
 cd  /home/ubuntu/addressbook
 docker build -t $1:$2 .
-docker run -itd -P $1:$2
+docker rm -f moneyuncle
+docker run -itd --name moneyuncle -p 8080:8080 $1:$2
